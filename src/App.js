7a3a0736762e7495418fda5a51ec7cef30dd70c8
minhaj abedin <minhajabedin341@gmail.com>
2021-02-23 12:44:47 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Component/Home/Home'
+function App() {
+  return (
+   <Home></Home>
+  );
+}
+
+export default App;
---
+++ src/Component/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header';
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
